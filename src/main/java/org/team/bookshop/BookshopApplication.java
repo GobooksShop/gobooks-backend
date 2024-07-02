453969a67478bfddda2f3a7e58cb5ed230da5411
[Cloud3] 오재영 <wodud.oh@gmail.com>
2024-07-02 18:19:19 +0900

    chore: dockerfile 추가

--- src/main/java/org/team/bookshop/BookshopApplication.java
+++ src/main/java/org/team/bookshop/BookshopApplication.java
@@ -1,5 +1,6 @@
 package org.team.bookshop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
