0f34a1323b36018fb9f50ca2ddea680669dbaf60
ruu <wjrdjs005@naver.com>
2024-07-03 00:56:32 +0900

    refector: portone 결제 관련 service 로직으로 변경
    컨트롤러 목 테스트를 하기 위해서 서비스로 분리했습니다.

--- src/main/java/org/team/bookshop/domain/payment/controller/PaymentController.java
+++ src/main/java/org/team/bookshop/domain/payment/controller/PaymentController.java
@@ -36,87 +36,25 @@ import org.team.bookshop.global.response.ApiResponse;
 public class PaymentController {
 
   private final PaymentService paymentService;
-  @Value("${PORTONE_REST_KEY}")
-  private String REST_API_KEY;
-
-  @Value("${PORTONE_SECRET_KEY}")
-  private String REST_KEY;
-
-  private IamportClient iamportClient;
-
-  private final OrderService orderService;
-
-  private final PaymentRepository paymentRepository;
-
-
-  @PostConstruct
-  public void init() {
-    this.iamportClient = new IamportClient(REST_API_KEY, REST_KEY);
-  }
 
   @PostMapping("/prepare")
   public ResponseEntity<? extends ApiResponse<?>> processPayment(
-      @RequestBody @Valid RequestPrevPayment requestPrevPayment)
-      throws IamportResponseException, IOException {
-    boolean isEqualsPrice = orderService.validateTotalPriceByMerchantId(
-        requestPrevPayment.getMerchantUid(), requestPrevPayment.getAmount());
+      @RequestBody @Valid RequestPrevPayment requestPrevPayment) {
 
-    if (!isEqualsPrice) {
-      log.warn("조작된 데이터이거나 주문 금액과 결제 금액이 다릅니다.");
-      return ResponseEntity.badRequest()
-          .body(ApiResponse.error(ErrorCode.ENTITY_NOT_FOUND, requestPrevPayment));
-    }
+    ResponsePrevPayment responsePrevPayment = paymentService.preparePayment(requestPrevPayment);
 
-    PrepareData prepareData = new PrepareData(requestPrevPayment.getMerchantUid(),
-        new BigDecimal(requestPrevPayment.getAmount()));
-    IamportResponse<Prepare> payment = iamportClient.postPrepare(prepareData);
-    log.info(String.valueOf(payment.getCode()));
-    //todo getCode에 따라 switch 문과 각 메서드는 service.() 특정 서비스로 변경 하는 것이 좋아보임
-    //todo 분기는 successPayment(), existPreventPayment()
-    if (payment.getCode() == 0) {
-      log.info("결제 요청 응답. 사전 결제 등록 번호: {}, 가격: {}", prepareData.getMerchant_uid(),
-          prepareData.getAmount().intValue());
-
-      return ResponseEntity.ok(ApiResponse.success(
-          ResponsePrevPayment.builder().merchantUid(prepareData.getMerchant_uid())
-              .amount(prepareData.getAmount().intValue()).build()));
-
-    } else if (payment.getCode() == 1) {
-      log.error("사전 결제 응답 요청 :{} {} 가 이미 존재합니다.", payment.getCode(), prepareData.getMerchant_uid());
-      return ResponseEntity.badRequest().body(ApiResponse.success(
-          ResponsePrevPayment.builder().merchantUid(prepareData.getMerchant_uid())
-              .amount(prepareData.getAmount().intValue()).build()));
-    } else {
-      log.error("사전 결제 요청 실패:{} {}", payment.getCode(), prepareData.getMerchant_uid());
-      return ResponseEntity.badRequest().body(ApiResponse.error(ErrorCode.PORTONE_BAD_REQUEST));
-    }
+    return ResponseEntity.ok(ApiResponse.success(
+        ResponsePrevPayment.builder()
+            .merchantUid(responsePrevPayment.getMerchantUid())
+            .amount(responsePrevPayment.getAmount())
+            .build()));
   }
 
   @PostMapping("/completePayment")
   public ResponseEntity<? extends ApiResponse<?>> processPayment(
-      @RequestBody @Valid RequestCompletePayment requestCompletePayment)
-      throws IamportResponseException, IOException {
+      @RequestBody @Valid RequestCompletePayment requestCompletePayment) {
 
-    Order order = orderService.findByMerchantUid(requestCompletePayment.getMerchantUid());
-
-    IamportResponse<Payment> paymentIamportResponse = iamportClient.paymentByImpUid(
-        requestCompletePayment.getImpUid());
-    Payment payment = paymentIamportResponse.getResponse();
-    int payment_totalPrice = payment.getAmount().intValue();
-    if (payment_totalPrice == order.getOrderTotalPrice()) {
-      //payment db에 저장.
-      log.info("===================================진입==================================");
-      paymentService.complatePayment(payment, order, requestCompletePayment.getMerchantUid());
-      System.out.println("페이먼트Repository에 저장 됐습니다.");
-    } else {
-      try {
-        CancelData cancelData = new CancelData(requestCompletePayment.getImpUid(), true);
-        iamportClient.cancelPaymentByImpUid(cancelData);
-      } catch (IamportResponseException ignored) {
-        CancelData cancelData = new CancelData(requestCompletePayment.getMerchantUid(), false);
-        iamportClient.cancelPaymentByImpUid(cancelData);
-      }
-    }
+    paymentService.complatePayment(requestCompletePayment);
     return ResponseEntity.ok(ApiResponse.success());
   }
 }
--- src/main/java/org/team/bookshop/domain/payment/dto/RequestCompletePayment.java
+++ src/main/java/org/team/bookshop/domain/payment/dto/RequestCompletePayment.java
@@ -2,9 +2,11 @@ package org.team.bookshop.domain.payment.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class RequestCompletePayment {
 
   @NotNull
