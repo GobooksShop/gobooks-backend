514fdf1053701aa2967130a861a977a385fb1af2
[Cloud3] 원다빈 <ynibad@gmail.com>
2024-07-04 15:09:18 +0900

    refactor: 불필요한 method, dto 정리

--- src/main/java/org/team/bookshop/domain/category/controller/CategoryPublicController.java
+++ src/main/java/org/team/bookshop/domain/category/controller/CategoryPublicController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.team.bookshop.domain.category.dto.CategoryChildrenResponseDto;
 import org.team.bookshop.domain.category.dto.CategoryDto;
 import org.team.bookshop.domain.category.dto.CategoryResponseDto;
 import org.team.bookshop.domain.category.service.CategoryService;
@@ -21,35 +20,21 @@ public class CategoryPublicController {
     this.categoryService = categoryService;
   }
 
-  @GetMapping("/categories")
-  public ResponseEntity<List<CategoryResponseDto>> getRootCategories() {
-    List<CategoryResponseDto> rootCategories = categoryService.getRootCategories();
-    return ResponseEntity.ok(rootCategories);
-  }
-
+  // READ
   @GetMapping("/categories/hierarchy")
   public ResponseEntity<List<CategoryResponseDto>> getCategoryHierarchy() {
     List<CategoryResponseDto> categoryHierarchy = categoryService.getCategoryHierarchy();
     return ResponseEntity.ok(categoryHierarchy);
   }
 
-  // READ
-  // 하위 계층을 모두 조회
-  @GetMapping("/categories/{categoryId}")
-  public ResponseEntity<CategoryResponseDto> getCategoryWithChildren(
-      @PathVariable Long categoryId) {
-    CategoryResponseDto responseDto = categoryService.getCategoryWithChildren(categoryId);
-    return ResponseEntity.ok(responseDto);
-  }
-
-  // 바로 아래 depth만 조회
-  @GetMapping("/categories/{categoryId}/children")
-  public ResponseEntity<List<CategoryChildrenResponseDto>> getCategoryWithDirectChildren(
-      @PathVariable Long categoryId) {
-    List<CategoryChildrenResponseDto> children = categoryService.getCategoryWithDirectChildren(
-        categoryId);
-    return ResponseEntity.ok(children);
-  }
+//  // 바로 아래 depth만 조회
+//  @GetMapping("/categories/{categoryId}/children")
+//  public ResponseEntity<List<CategoryChildrenResponseDto>> getCategoryWithDirectChildren(
+//      @PathVariable Long categoryId) {
+//    List<CategoryChildrenResponseDto> children = categoryService.getCategoryWithDirectChildren(
+//        categoryId);
+//    return ResponseEntity.ok(children);
+//  }
 
   // BreadCrumb
   @GetMapping("/categories/{categoryId}/breadcrumbs")
--- src/main/java/org/team/bookshop/domain/category/entity/Category.java
+++ src/main/java/org/team/bookshop/domain/category/entity/Category.java
@@ -1,7 +1,6 @@
 package org.team.bookshop.domain.category.entity;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
